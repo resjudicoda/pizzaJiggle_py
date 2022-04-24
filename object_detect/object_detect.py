8243001cd81b5fc7c18022e796cce780147a99fb
resjudicoda <adam.j.reis@gmail.com>
2022-04-24 12:14:26 -0700

    set up virtual environment

--- object_detect/object_detect.py
+++ object_detect/object_detect.py
@@ -1,12 +1,20 @@
+from torchvision.models import detection
+import numpy as np
 import torch
-x = torch.rand(5, 3)
-print(x)
+import cv2 as cv
+import argparse
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
+args = vars(ap.parse_args())
+
+img = cv.imread(args["image"])
+cv.imshow("image", img)
+
+if img is None:
+    sys.exit("Could not read the image.")
 
 
-# from torchvision.models import detection
-# import numpy as np
-# import torch
-# import cv2
 
-# model = detection.retinanet_resnet50_fpn(pretrained=True)
-# model.eval()
+model = detection.retinanet_resnet50_fpn(pretrained=True)
+model.eval()
